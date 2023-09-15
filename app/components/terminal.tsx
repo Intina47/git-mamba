724404e6e720ebb8b9ce759f013d9a7a2c2e0366
uzohou <isaiahntina47@gmail.com>
2023-09-15 09:56:13 +0100

    who am i

--- app/components/terminal.tsx
+++ app/components/terminal.tsx
@@ -2,7 +2,7 @@
 import React, { useEffect, useState } from 'react';
 import { useInterval } from '../utils/blinkerIntervals';
 import BootUpSequence from './bootupSequence';
-import {helpCommands} from '../constants';
+import { helpCommands } from '../constants';
 
 const TerminalLayout = () => {
  const [blink, setBlink] = useState(false);
@@ -12,35 +12,58 @@ const TerminalLayout = () => {
  const [inputValue, setInputValue] = useState('');
  const [outputText, setOutputText] = useState<React.ReactNode[]>([]);
  const endOfTerminalRef = React.useRef<HTMLDivElement>(null);
+ const [readReadmeHtml, setReadmeHtml] = useState('');
 
- // scroll to the end of the terminal
+ // Scroll to the end of the terminal
  const scrollToBottom = () => {
   if (endOfTerminalRef.current) {
-   endOfTerminalRef.current.scrollIntoView({ behavior: 'smooth', block: 'end'});
+   endOfTerminalRef.current.scrollIntoView({ behavior: 'smooth', block: 'end' });
   }
  };
- //  clear terminal
+
+ // Clear terminal
  const clearTerminal = () => {
   setOutputText([]);
  };
-  // scroll to the end of the terminal when the outputText changes
+
+ useEffect(() => {
+  async function fetchReadme() {
+   try {
+    const response = await fetch('/api/readme');
+    console.log('Fetching from read me');
+    if (response.ok) {
+     console.log('fetch was succefull');
+     const data = await response.json();
+     setReadmeHtml(data.htmlContent);
+    } else {
+     console.error('[loop] Error fetching README: ', response.statusText);
+    }
+   } catch (error) {
+    console.error('[catch] Error fetching README: ', error);
+   }
+  }
+  fetchReadme();
+ }, []);
+ // Scroll to the end of the terminal when the outputText changes
  useEffect(() => {
   scrollToBottom();
- }
- ,[outputText]);
- //  simulate the boot-up process
+ }, [outputText]);
+
+ // Simulate the boot-up process
  useEffect(() => {
   if (typeof window !== 'undefined') {
-   // retrieve the count from the localStorage
+   // Retrieve the count from localStorage
    const storedUserCount = localStorage.getItem('userCount');
    if (storedUserCount) {
     setUserCount(parseInt(storedUserCount, 10));
    } else {
     setUserCount(0);
    }
+
    // Get the timestamp when the component first loads
    const loginTime = new Date().toUTCString();
    setLastLoginTime(loginTime);
+
    // Increment the user count and store it
    const newUserCount = userCount + 1;
    setUserCount(newUserCount);
@@ -49,26 +72,29 @@ const TerminalLayout = () => {
    const timer = setTimeout(() => {
     setBooting(false);
    }, 5000);
+
    return () => clearTimeout(timer);
   }
- },[]);
+ }, []);
 
  useInterval(() => {
   setBlink(!blink);
- }
- , 500);
- // handle user input
- const handleInputChange = (e: { target: { value: React.SetStateAction<string>; }; }) => {
+ }, 500);
+
+ // Handle user input
+ const handleInputChange = (e: { target: { value: React.SetStateAction<string> } }) => {
   setInputValue(e.target.value);
  };
- // handle user input submission
- const handleInputSubmit = (e: { preventDefault: () => void; }) => {
+
+ // Handle user input submission
+ const handleInputSubmit = (e: { preventDefault: () => void }) => {
   e.preventDefault();
   if (inputValue.trim() === '') return;
 
-  // create a unique key for the new jsx element
+  // Create a unique key for the new JSX element
   const uniqueKey = new Date().getTime().toString();
-  // add the user input as a string
+
+  // Add the user input as a string
   const inputString = (
    <p className='text-white font-mono' key={uniqueKey + '-input'}>
     <span className='text-red-500'>guest</span>
@@ -81,31 +107,51 @@ const TerminalLayout = () => {
 
   // Combine both arrays and set the state
   setOutputText((prevOutput) => [...prevOutput, inputString]);
-  // handle user input
-  if (inputValue.trim() === 'help') {
-   const helpOutput: React.ReactNode[] = [];
-   helpOutput.push(<div key="help-heading" className="text-yellow-500 font-light text-sm mb-1">Use the commands below to get around:</div>);
-   helpCommands.forEach((command) => {
-    helpOutput.push(
-     <div key={command.name} className="text-white font-light text-sm">
-      <span className="text-yellow-500">{command.name}</span> - {command.description}
-     </div>
-    );
-   });
-   setOutputText((prevOutput) => [...prevOutput, ...helpOutput]);
-  } else if (inputValue.trim() === 'clear'){
+  let helpString: string | number | boolean | React.JSX.Element | Iterable<React.ReactNode> | React.PromiseLikeOfReactNode | null | undefined;
+  let errorString: string | number | boolean | React.JSX.Element | Iterable<React.ReactNode> | React.PromiseLikeOfReactNode | null | undefined;
+  let aboutmeString: string | number | boolean | React.JSX.Element | Iterable<React.ReactNode> | React.PromiseLikeOfReactNode | null | undefined;
+
+  // Handle user input using a switch
+  switch (inputValue) {
+  case 'help':
+   helpString = (
+    <div className='text-white text-sm font-light' key={uniqueKey + '-output'}>
+     <p className='text-sm text-green-500 font-light mb-1'>use the commands below to get around:</p>
+     <ul className='list-disc list-inside'>
+      {helpCommands.map((command, index) => (
+       <li key={index}>
+        <span className='text-yellow-500'>{command.name}</span>
+        <span className='text-white'> - {command.description}</span>
+       </li>
+      ))}
+     </ul>
+    </div>
+   );
+   setOutputText((prevOutput) => [...prevOutput, helpString]);
+   break;
+  case 'clear':
    clearTerminal();
-  } else {
-   const errorString = (
-    <p className='text-red-500 font-mono' key={uniqueKey + '-error'}>
+   break;
+  case 'whoami':
+   aboutmeString = (
+    <div dangerouslySetInnerHTML={{__html: readReadmeHtml}} />
+   );
+   setOutputText((prevOutput) => [...prevOutput, aboutmeString]);
+   break;
+  default:
+   errorString = (
+    <p className='text-red-500 font-mono' key={uniqueKey + '-output'}>
      {inputValue}: command not found
     </p>
    );
    setOutputText((prevOutput) => [...prevOutput, errorString]);
+   break;
   }
-  // clear the input field
+
+  // Clear the input field
   setInputValue('');
  };
+
  // Function to format the user count as "imnssxxx"
  const formatUserCount = () => {
   const paddedCount = String(userCount).padStart(3, '0');
@@ -113,52 +159,58 @@ const TerminalLayout = () => {
  };
 
  return (
-  <div className="w-full md:w-1/2 bg-black text-green-500 p-6 rounded-lg shadow-lg mx-auto">
-   {
-    booting ? <BootUpSequence /> :
-     <>
-      {/* mamba.sh heading */}
-      <p className="text-green-500 text-2xl font-mono">mamba.sh</p>
+  <div className='w-full md:w-1/2 bg-black text-green-500 p-6 rounded-lg shadow-lg mx-auto'>
+   {booting ? (
+    <BootUpSequence />
+   ) : (
+    <>
+     {/* mamba.sh heading */}
+     <p className='text-green-500 text-2xl font-mono'>mamba.sh</p>
 
-      <hr className="border-gray-600 my-4"/>
-      <p className="text-gray-600 text-xs font-mono mb-1">Last login: {lastLoginTime} on {formatUserCount()} </p>
-      <p className="text-white font-mono">Hi!</p>
-      <p className="text-white font-mono mb-4">I&apos;m Ntina, am currently a 4th year computing student at the Univerisity of Dundee</p>
-      <p className="text-white font-mono">Type <span className="text-yellow-500 px-1 rounded">&lsquo;help&lsquo;</span> to see a list of commands.</p>
-      <hr className="border-gray-600 my-4"/>
+     <hr className='border-gray-600 my-4' />
+     <p className='text-gray-600 text-xs font-mono mb-1'>
+            Last login: {lastLoginTime} on {formatUserCount()}{' '}
+     </p>
+     <p className='text-white font-mono'>Hi!</p>
+     <p className='text-white font-mono mb-4'>
+            I&apos;m Ntina, am currently a 4th year computing student at the Univerisity of Dundee
+     </p>
+     <p className='text-white font-mono'>
+            Type <span className='text-yellow-500 px-1 rounded'>&lsquo;help&lsquo;</span> to see a list of commands.
+     </p>
+     <hr className='border-gray-600 my-4' />
 
-      {/* Display terminal output */}
-      <div className="mb-2" ref={endOfTerminalRef}>
-       {outputText.map((line, index) => (
-        <p key={index} className="text-white font-mono">
-         {line}
-        </p>
-       ))}
+     {/* Display terminal output */}
+     <div className='mb-2'>
+      {outputText.map((line, index) => (
+       <p key={index} className='text-white font-mono'>
+        {line}
+       </p>
+      ))}
+     </div>
+
+     <form onSubmit={handleInputSubmit}>
+      {/* md:flex-col md:flex-wrap md:items-baseline */}
+      <div className='flex items-baseline' ref={endOfTerminalRef}>
+       <p className='text-white font-mono'>
+        <span className='text-red-500'>guest</span>
+        <span className='text-yellow-500'>@</span>
+        <span className='text-blue-500'>mamba.sh</span>
+        <span className='text-yellow-500'> ~ $</span>
+       </p>
+       <input
+        type='text'
+        className='bg-transparent text-white outline-none ml-2'
+        value={inputValue}
+        onChange={handleInputChange}
+        autoFocus
+       />
       </div>
-
-      <form onSubmit={handleInputSubmit}>
-       {/* md:flex-col md:flex-wrap md:items-baseline */}
-       <div className="flex items-baseline" ref={endOfTerminalRef}>
-        <p className="text-white font-mono">
-         <span className="text-red-500">guest</span>
-         <span className="text-yellow-500">@</span>
-         <span className="text-blue-500">mamba.sh</span>
-         <span className="text-yellow-500"> ~ $</span>
-        </p>
-        <input
-         type="text"
-         className="bg-transparent text-white outline-none ml-2"
-         value={inputValue}
-         onChange={handleInputChange}
-         autoFocus
-        />
-       </div>
-      </form>
-     </>
-   }
+     </form>
+    </>
+   )}
   </div>
  );
-
 };
 
 export default TerminalLayout;
---
+++ public/README.md
@@ -0,0 +1,14 @@
+## Language Breakdown
+![Top Languages](https://github-readme-stats.vercel.app/api/top-langs/?username=Intina47&layout=compact&hide=html)
+![Your Name's GitHub Stats](https://github-readme-stats.vercel.app/api?username=Intina47&show_icons=true&theme=radical&exclude_repo=contributed&hide=contribs)
+
+### Hi there 👋
+I'm Ntina, I have a keen interest in exploring various fields of computer science, and currently, I'm focused on expanding my knowledge in machine learning.
+
+💡 This GitHub profile serves as a space where I share my projects, experiments, and learnings. Feel free to explore my repositories and provide feedback or suggestions. Let's connect and learn together!
+
+📫 You can reach me via email at isaiahntina47@gmail.com. I'm always open to discussions, collaborations, and learning opportunities.
+
+Let's embrace the journey of continuous learning and exploration in the fascinating world of computer science and machine learning! 🚀
+
+
