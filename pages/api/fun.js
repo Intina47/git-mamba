4713846d2d0ab440d693920f70a454efdb421b22
uzohou <isaiahntina47@gmail.com>
2023-09-18 20:33:55 +0100

    puppe

--- pages/api/fun.js
+++ pages/api/fun.js
@@ -23,7 +23,7 @@ export default async function handler(req, res) {
  const randomAnimal = Animals[randomIndex];
  try {
   const result = await queue.add(async () => {
-   const browser = await puppeteer.launch();
+   const browser = await puppeteer.launch({ headless: 'new' });
    const page = await browser.newPage();
    const url = `https://www.pinterest.co.uk/search/pins/?q=${randomAnimal}`;
    await page.goto(url);
