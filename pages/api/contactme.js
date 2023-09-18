ef67751413d5035f4727916e5fc52e741ef33e3b
uzohou <isaiahntina47@gmail.com>
2023-09-18 19:58:55 +0100

    new const

--- pages/api/contactme.js
+++ pages/api/contactme.js
@@ -4,12 +4,12 @@ import path from 'path';
 import MarkdownIt from 'markdown-it';
 const md = new MarkdownIt();
 export default async (req, res) => {
-    try{
-        let data= await fs.readFile(path.join(__dirname,'../contactme.md'),"utf8");
-        let htmlContent = md.render(data);
-        res.status(200).json({htmlContent});
-    }catch(error){
-        console.error('Error reading contactme.md: ', error);
-        res.status(500).json({error: 'Error while reading the contactme.md file'});
-    }
-}
+ try{
+  let data= await fs.readFile(path.join(__dirname,'../contactme.md'),'utf8');
+  let htmlContent = md.render(data);
+  res.status(200).json({htmlContent});
+ }catch(error){
+  console.error('Error reading contactme.md: ', error);
+  res.status(500).json({error: 'Error while reading the contactme.md file'});
+ }
+};
