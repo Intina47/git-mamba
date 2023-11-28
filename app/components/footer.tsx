37ff744e4a6cf1950da1421700bb965d85e869b7
uzohou <isaiahntina47@gmail.com>
2023-11-28 01:04:18 +0000

    Add footer component and styles for mobile

---
+++ app/components/footer.tsx
@@ -0,0 +1,48 @@
+// path: app/components/footer.tsx
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const YOUR_PAT = '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';
+
+type CommitData = {
+    latestCommit: string;
+    commitmessage: string;
+};
+
+const Footer = () => {
+ const [Commit, setLatestCommit] = useState<CommitData>({ latestCommit: '', commitmessage: '' });
+
+ useEffect(() => {
+  const fetchLatestCommit = async () => {
+   try {
+    const response = await axios.get('/api/latest-commit', {
+     headers: {
+      Authorization: `Bearer ${YOUR_PAT}`,
+     },
+    });
+    setLatestCommit(response.data);
+   } catch (error) {
+    throw new Error(`Error: ${error}`);
+   }
+  };
+
+  fetchLatestCommit();
+ }, []);
+
+ return (
+  <div className="footerm">
+   <div className="footer-left">
+    <p className="text-white">Current Project, Github Latest Commit</p>
+    <p className="text-yellow-500">
+     <a id='latestrepo' href="https://github.com/Intina47/git-mamba" className="no-underline text-current">
+      {Commit.latestCommit.slice(0, 12)}
+     </a>
+      :{' '}
+     <span className="text-white">{Commit.commitmessage.length > 10 ? `${Commit.commitmessage.slice(0, 10)}...` : Commit.commitmessage}</span>
+    </p>
+   </div>
+  </div>
+ );
+};
+
+export default Footer;
