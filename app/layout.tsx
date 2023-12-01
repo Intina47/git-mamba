af5c8bc5644850f39893e861ba4123ba9b5f9147
uzohou <isaiahntina47@gmail.com>
2023-12-01 12:07:50 +0000

    Add Analytics component to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
+import { Analytics } from '@vercel/analytics/react';
 import Head from 'next/head';
 
 const inter = Inter({ subsets: ['latin'] });
@@ -22,7 +23,10 @@ export default function RootLayout({
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <link rel="icon" href="./favicon.ico" />
    </Head>
-   <body className={inter.className}>{children}</body>
+   <body className={inter.className}>
+    {children}
+    <Analytics />
+   </body>
   </html>
  );
 }
