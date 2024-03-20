556036a53acdb866f71802e48a32d048ddf6489e
mamba <isaiahntina47@gmail.com>
2024-03-20 14:59:57 +0000

    Refactor languagestats.js API handler

--- pages/api/languagestats.js
+++ pages/api/languagestats.js
@@ -2,31 +2,18 @@ import fs from 'fs/promises';
 import path from 'path';
 import MarkdownIt from 'markdown-it';
 
-export default async function handler(res){
- try{
-  const statsPath = path.resolve(process.cwd(), 'public/languageStats.md');
-  const stats = await fs.readFile(statsPath, 'utf-8');
-
-  const md = new MarkdownIt();
-  const htmlContent = md.render(stats);
-  console.log('========GOT STATS===========');
-  console.log(htmlContent);
-  console.log('========GOT STATS===========');
-
-  if (res && typeof res.status === 'function') {
-   res.status(200).json({htmlContent});
-  } else {
-   console.log('Error Getting stats:=========\n', 'Invalid response object');
-   res.status(500).json({error: 'Invalid response object'});
+export default async function handler(req,res) {
+   try{
+    const readmePath = path.resolve(process.cwd(), 'public/languageStats.md');
+    const readme = await fs.readFile(readmePath, 'utf-8');
+  
+    // use markdown html to reander the readme content to html
+    const md = new MarkdownIt();
+    const htmlContent = md.render(readme);
+  
+    res.status(200).json({htmlContent});
+   } catch (error) {
+    console.error('Error reading README.md: ', error);
+    res.status(500).json({error: 'Error while reading the README.md file'});
+   }
   }
- }catch (error){
-  console.log('Error Getting stats:=========\n', error);
-
-  if (res && typeof res.status === 'function') {
-   res.status(500).json({error: 'Error reading stats'});
-  } else {
-   console.log('Error Getting stats:=========\n', 'Invalid response object');
-   res.status(500).json({error: 'Invalid response object'});
-  }
- }
-}
