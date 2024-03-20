881c2155448678eeac6ce8dd4ead61de4a535951
mamba <isaiahntina47@gmail.com>
2024-03-20 14:52:11 +0000

    Add Nanéu, a discord jobs bot, to the footer

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -31,8 +31,21 @@ const Footer = () => {
 
  return (
   <div className="footerm">
+  {/* add my nanéu, my discord jobs bot to your server */}
+  <div className='footer-right mt-2 mb-2'>
+    <h1 className="text-white text-md font-bold">Meet Nanéu,</h1>
+    <p className="text-white mb-1 text-sm">A discord jobs bot. Invite Nanéu to your server and add it to your jobs channel. You can configure it to your own job preferences.</p>
+    <p className="text-yellow-500 text-sm">
+     <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded' type='button'>           
+     <a href="https://discord.com/oauth2/authorize?client_id=1217949022275506176" className='no-underline text-white' target="_blank" rel="noopener noreferrer">
+                Invite Nanéu to your server
+            </a>
+        </button>
+    </p>
+
+    </div>
    <div className="footer-left">
-    <p className="text-white mb-1">Current Project, Latest Github Commit</p>
+    <p className="text-white mb-1 text-sm">Current Project, Latest Github Commit</p>
     <p className="text-yellow-500 text-sm">
      <a id='latestrepo' href="https://github.com/Intina47/EE_error" className="no-underline text-current">
       {Commit.latestCommit.slice(0, 12)}
