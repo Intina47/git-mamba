a5055484f19c4750db59212dd0703a94069979d9
mamba <isaiahntina47@gmail.com>
2024-04-11 17:01:27 +0000

    Add QuoteForm component to pages directory

---
+++ pages/quoteform.js
@@ -0,0 +1,10 @@
+import React from 'react';
+// '../utils/blinkerIntervals';
+
+import QuoteForm from '../app/components/admin/QuoteForm';
+// import css
+import '../app/globals.css';
+
+export default function QuoteFormPage() {
+ return <QuoteForm />;
+}
