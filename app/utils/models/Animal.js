f1ef26b2415d0ea755958d75679bf6622df5f295
mamba <isaiahntina47@gmail.com>
2024-04-15 04:01:01 +0000

    Refactor Animal model and remove unused Quote model

--- app/utils/models/Animal.js
+++ app/utils/models/Animal.js
@@ -14,18 +14,7 @@ const animalSchema = new mongoose.Schema({
  },
 });
 
-const quoteSchema = new mongoose.Schema<Quote>({
- quote: {
-  type: String,
-  required: true,
- },
- source: {
-  type: String,
- },
- sourceUrl: {
-  type: String,
- },
-});
 
-export const QuoteModel = mongoose.models.Quote || mongoose.model('quotes', quoteSchema);
-export const animalModel = mongoose.models.Animal || mongoose.model('animals', animalSchema);
+const animalModel = mongoose.models.Animal || mongoose.model('animals', animalSchema);
+
+export { animalModel };
