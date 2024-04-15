4f16ba40661d37bd9516b853bd994e19b029f217
mamba <isaiahntina47@gmail.com>
2024-04-15 04:01:16 +0000

    Add API endpoint for saving quotes

---
+++ pages/api/quotes.js
@@ -0,0 +1,21 @@
+import {connectToDatabase, disconnectFromDatabase} from 'app/utils/db.js';
+import { quoteModel } from 'app/utils/models/Quote.js';
+
+export default async function handler(req, res) {
+    try {
+    await connectToDatabase();
+    const { method } = req;
+    if(method === 'POST'){
+    const newQuote = new quoteModel(req.body);
+    await newQuote.save();
+    await disconnectFromDatabase();
+    res.status(201).json(newQuote);
+    } else {
+    res.setHeader('Allow', ['POST']);
+    res.status(405).json({error: `Method ${method} Not Allowed`});
+    }
+    } catch (error) {
+    console.error('Error occured saving quote:', error);
+    res.status(500).json({error: 'Error saving quote'});
+    }
+    }
