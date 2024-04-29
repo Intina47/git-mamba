07101e59e9096b54a6f493542e5ce0e93bb3a6b0
mamba <isaiahntina47@gmail.com>
2024-04-29 20:03:27 +0000

    Add NotesPage component to display notes dashboard

---
+++ pages/notes.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import NotesDash from '../app/components/notes/notesDash';
+import '../app/globals.css';
+
+export default function NotesPage() {
+ return (
+  <NotesDash />
+ );
+}
